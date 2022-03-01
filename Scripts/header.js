201b46cb85a8f19bd81d44663cc520c55fdebf3c
Ukungazi <titonictitan@gmail.com>
2022-02-28 19:00:05 -0600

    Google Analytics Part 2

--- Scripts/header.js
+++ Scripts/header.js
@@ -1,22 +1,9 @@
-class Header extends HTMLElement {
-    constructor() {
-      super();
-    }
-  
-    connectedCallback() {
-      this.innerHTML = `
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-62XQ7TYLDW"></script>
-      <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      
-      gtag('config', 'G-62XQ7TYLDW');
-      </script>
-      `;
-    }
-  }
-  
-  customElements.define('header-component', Header);
+var script = document.createElement("script");  // create a script DOM node
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-62XQ7TYLDW";
+document.head.appendChild(script);
 
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
+gtag('config', 'G-62XQ7TYLDW');
